a5ac9f39105f08b61540494d9331e476b5436250
Haekalsss <kallsss145@gmail.com>
2025-07-07 14:44:03 +0700

    hahh

--- config/cors.go
+++ config/cors.go
@@ -4,7 +4,7 @@ var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://example.com",
 	"http://127.0.0.1:8088",
-	"https://frontend-mahasiwa-47l1.vercel.app",
+	"https://frontend-mahasiwa-47l1.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
